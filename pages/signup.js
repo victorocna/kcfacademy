9d574e86453a366a2d0fe00b6287ed3135a3627e
victorocna <victorocnarescu@gmail.com>
2023-09-21 15:06:44 +0300

    remove slider

--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,4 @@
-import { MainHeader, Footer, Signup, Slider } from '../components';
+import { MainHeader, Footer, Signup } from '../components';
 
 const Page = () => (
   <>
@@ -6,13 +6,8 @@ const Page = () => (
       <MainHeader />
     </header>
     <main className="cover bg-gray-200 md:bg-gray-600 flex flex-col items-center justify-center pt-4 flex-1">
-      <div className="max md:my-8 md:grid gap-8 grid-cols-2">
-        <div>
-          <Signup />
-        </div>
-        <div className="screenshot-slider flex flex-col items-center my-4 md:my-0 md:rounded overflow-hidden">
-          <Slider />
-        </div>
+      <div className="flex max-md">
+        <Signup />
       </div>
     </main>
     <Footer />
