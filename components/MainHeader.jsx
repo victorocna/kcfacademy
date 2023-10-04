9ffa5e374d404aa1516e17cf2d7e735f438f043a
Claudiu <claudeady4579@gmail.com>
2023-10-04 21:16:56 +0300

    add logo to the main page

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -5,6 +5,7 @@ const MainHeader = () => (
     <div className="max flex flex-wrap items-center">
       <img className="logo-app" src={logo} alt={title} />
       <h1 className="font-display text-lg md:text-xl font-semibold text-bluemarin">{title}</h1>
+      <img className="w-16" src="/images/kcf-logo-alt.png" alt={title} />
     </div>
   </div>
 );
