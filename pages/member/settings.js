9f98adb4e2fa05b8e2df16b34b18dc3519dd12fc
victorocna <victorocnarescu@gmail.com>
2023-10-24 18:53:14 +0300

    add change password

--- pages/member/settings.js
+++ pages/member/settings.js
@@ -1,16 +1,21 @@
 import { Profile, withAuth } from 'echess/components';
+import { ChangePasswordFormik } from 'echess/components/Identity';
 import { Account } from 'echess/components/Member';
 import { Layout } from '../../components';
 
 const Page = () => (
   <Layout role="member">
-    <div className="bg-white border mb-4 rounded-lg shadow py-8">
+    <div className="bg-white border rounded-lg shadow py-8 mb-4">
       <h2 className="font-bold text-2xl mb-4 px-4 lg:px-8">Account settings</h2>
       <Profile />
     </div>
-    <div className="bg-white border rounded-lg shadow py-8">
+    <div className="bg-white border rounded-lg shadow py-8 mb-4">
       <h2 className="font-bold text-2xl mb-4 px-4 lg:px-8">Personal info</h2>
       <Account />
+    </div>
+    <div className="bg-white border rounded-lg shadow py-8 mb-4">
+      <h2 className="font-bold text-2xl mb-4 px-4 lg:px-8">Change password</h2>
+      <ChangePasswordFormik />
     </div>
   </Layout>
 );
--- pages/teacher/settings.js
+++ pages/teacher/settings.js
@@ -1,11 +1,16 @@
 import { Profile, withAuth } from 'echess/components';
+import { ChangePasswordFormik } from 'echess/components/Identity';
 import { Layout } from '../../components';
 
 const Page = () => (
   <Layout role="teacher">
-    <div className="bg-white border rounded-lg shadow py-8">
+    <div className="bg-white border rounded-lg shadow py-8 mb-4">
       <h2 className="font-bold text-2xl mb-4 px-4 lg:px-8">Account settings</h2>
       <Profile />
+    </div>
+    <div className="bg-white border rounded-lg shadow py-8 mb-4">
+      <h2 className="font-bold text-2xl mb-4 px-4 lg:px-8">Change password</h2>
+      <ChangePasswordFormik />
     </div>
   </Layout>
 );
