77d918b90ef7fd63e3fc4788d84bd99a338c0498
victorocna <victorocnarescu@gmail.com>
2024-01-04 15:46:44 +0200

    remove lightbox

--- components/index.js
+++ components/index.js
@@ -3,14 +3,12 @@ import Footer from './Footer';
 import Forgot from './Forgot';
 import Header from './Header';
 import Layout from './Layout';
-import Lightbox from './Lightbox';
 import Link from './Link';
 import Login from './Login';
 import MainHeader from './MainHeader';
 import Maintenance from './Maintenance';
 import Reset from './Reset';
 import Signup from './Signup';
-import Slider from './Slider';
 import ThankYou from './ThankYou';
 import TopHeader from './TopHeader';
 
@@ -20,14 +18,12 @@ export {
   Forgot,
   Header,
   Layout,
-  Lightbox,
   Link,
   Login,
   MainHeader,
   Maintenance,
   Reset,
   Signup,
-  Slider,
   ThankYou,
   TopHeader,
 };
